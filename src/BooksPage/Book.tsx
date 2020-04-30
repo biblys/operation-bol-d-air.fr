1c1665121751c564c695806a88e95132f145f35c
Clément Bourgoin <c@iwzr.fr>
2020-04-30 10:38:25 +0200

    Re-add download button for available books

--- src/BooksPage/Book.tsx
+++ src/BooksPage/Book.tsx
@@ -7,12 +7,19 @@ import mysteryBook from './mystery-book.jpg';
 export default function({
   startDate,
   endDate,
+  downloadLink,
   coverImageUrl,
   altText,
   paperEan
 }: Book) {
   const buyLink = `https://www.placedeslibraires.fr/livre/${paperEan}/`;
-  const started = startDate.isSameOrBefore();
+  const started = startDate.isSameOrBefore()
+
+  const ended = endDate.isBefore(undefined, 'day'); // If end date is before today
+  const downloadLinkWithTags = `${downloadLink}?utm_source=operation-bol-d-air&utm_medium=clic&utm_campaign=bol-d-air-${startDate
+    .format('YYYY-MM-DD')
+    .replace(' ', '-')}`;
+
 
   return (
     <div className="Book">
@@ -33,12 +40,26 @@ export default function({
           href={buyLink}
           target="_blank"
           rel="noopener noreferrer"
-          aria-label={`Télécharger ${altText}`}
+          aria-label={`Acheter ${altText}`}
         >
-          <span aria-label="cadeau" role="img">
+          <span aria-label="cœur" role="img">
             ♥️
           </span>{' '}
           <strong>Acheter</strong>
+        </a>
+      ) : null}
+      {started && !ended ? (
+        <a
+          className="button green"
+          href={downloadLinkWithTags}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label={`Télécharger ${altText}`}
+        >
+          <span aria-label="cadeau" role="img">
+            🎁
+          </span>{' '}
+          <strong>Télécharger</strong>
         </a>
       ) : null}
     </div>
