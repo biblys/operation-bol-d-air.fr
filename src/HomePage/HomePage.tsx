0213b3b33df0519cf7b1853f91599753b843641e
Clément Bourgoin <c@iwzr.fr>
2020-06-17 12:18:14 +0200

    Fix unused vars in HomePage component

--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import backgroundResizer from '../lib/backgroundResizer';
-//@ts-ignore
-import { Link } from 'react-scroll';
 
 import './HomePage.css';
 
-const backgroundImageFile = 'home-page-background.jpg';
-
 export default function HomePage() {
   return (
-    <div
-      id="home"
-      className="HomePage page"
-    >
+    <div id="home" className="HomePage page">
       <header className="header">
         <h1 className="page-title">L'Opération Bol d'air, c'est fini !</h1>
       </header>
